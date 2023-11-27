aaf69492a15ba6cd8c67d507112e7edb32dd000a
Jevgenijs Kozuhovs <jkozuhovs@gmail.com>
2023-11-27 05:25:38 +0200

    broken after the thirdbanner.js edit, the object of banners is a problem

--- src/components/ThirdBanner.js
+++ src/components/ThirdBanner.js
@@ -1,14 +1,27 @@
 import './ThirdBanner.css';
 import React from 'react';
+import banner1 from '../assets/ThirdBanner1.png';
+import banner2 from '../assets/ThirdBanner2.png';
+import banner3 from '../assets/ThirdBanner3.png';
+import banner4 from '../assets/ThirdBanner4.png';
+import banner5 from '../assets/ThirdBanner5.png';
+import banner6 from '../assets/ThirdBanner6.png';
 
-const banners = [1,2,3,4,5,6];
+const banners = {
+    1: banner1,
+    2: banner2,
+    3: banner3,
+    4: banner4,
+    5: banner5,
+    6: banner6
+}
 
 function ThirdBanner(){
     return(
         <>
             <div id="thirdBannerWrapper">
                 {banners.map((number)=>(
-                    <div style={{backgroundImage: `url(../assets/ThirdBanner${number}.png)`}} className={`thirdBannerSection thirdBanner${number}`}></div>
+                    <div style={{backgroundImage: `url(${banners[number]})`}} className={`thirdBannerSection thirdBanner${number}`}></div>
                 ))}
                
             </div>
