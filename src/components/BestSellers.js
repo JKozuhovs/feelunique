a32171562d6da04508e399263398a3812959ffa1
Jevgenijs Kozuhovs <jkozuhovs@gmail.com>
2023-12-17 10:01:06 +0200

    did so the best sellers items load from the bestsellers.js, for now it's manual input in there

--- src/components/BestSellers.js
+++ src/components/BestSellers.js
@@ -3,7 +3,55 @@ import React, {useState, useEffect} from 'react';
 import MakeupItems from './MakeupItems';
 import SkinItems from './SkinItems';
 import FragnanceItems from './FragnanceItems';
+import item0 from '../assets/makeup_items/item0.PNG';
+import item1 from '../assets/makeup_items/item1.PNG';
+import item2 from '../assets/makeup_items/item2.PNG';
+import item3 from '../assets/makeup_items/item3.PNG';
+import item4 from '../assets/makeup_items/item4.PNG';
+import item5 from '../assets/makeup_items/item5.PNG';
+import item6 from '../assets/makeup_items/item6.PNG';
+import item7 from '../assets/makeup_items/item7.PNG';
+import item8 from '../assets/makeup_items/item8.PNG';
+import item9 from '../assets/makeup_items/item9.PNG';
 
+const items = [
+    { id: '0', source: item0, price: "13.76", name: "INGLOT AMC Eyerliner Gel 5.5g", heartState: 'empty_heart' },
+    { id: '1', source: item1, price: "15.20", name: "L'Oréal Paris Matte Lipstick", heartState: 'empty_heart' },
+    { id: '2', source: item2, price: "9.99", name: "Revlon ColorStay Foundation", heartState: 'empty_heart' },
+    { id: '3', source: item3, price: "12.50", name: "Maybelline Sky High Mascara", heartState: 'empty_heart' },
+    { id: '4', source: item4, price: "7.30", name: "NYX Professional Eyeshadow Palette", heartState: 'empty_heart' },
+    { id: '5', source: item5, price: "18.40", name: "Estée Lauder Double Wear Concealer", heartState: 'empty_heart' },
+    { id: '6', source: item6, price: "20.00", name: "Fenty Beauty Pro Filt'r Primer", heartState: 'empty_heart' },
+    { id: '7', source: item7, price: "10.99", name: "Urban Decay Setting Spray", heartState: 'empty_heart' },
+    { id: '8', source: item8, price: "16.75", name: "Huda Beauty Liquid Matte Lipstick", heartState: 'empty_heart' },
+    { id: '9', source: item9, price: "21.99", name: "NARS Radiant Creamy Concealer", heartState: 'empty_heart' }
+];
+
+const itemsSecond = [
+    { id: '0', source: item9, price: "13.76", name: "INGLOT AMC Eyerliner Gel 5.5g", heartState: 'empty_heart' },
+    { id: '1', source: item2, price: "15.20", name: "L'Oréal Paris Matte Lipstick", heartState: 'empty_heart' },
+    { id: '2', source: item1, price: "9.99", name: "Revlon ColorStay Foundation", heartState: 'empty_heart' },
+    { id: '3', source: item3, price: "12.50", name: "Maybelline Sky High Mascara", heartState: 'empty_heart' },
+    { id: '4', source: item6, price: "7.30", name: "NYX Professional Eyeshadow Palette", heartState: 'empty_heart' },
+    { id: '5', source: item5, price: "18.40", name: "Estée Lauder Double Wear Concealer", heartState: 'empty_heart' },
+    { id: '6', source: item4, price: "20.00", name: "Fenty Beauty Pro Filt'r Primer", heartState: 'empty_heart' },
+    { id: '7', source: item7, price: "10.99", name: "Urban Decay Setting Spray", heartState: 'empty_heart' },
+    { id: '8', source: item8, price: "16.75", name: "Huda Beauty Liquid Matte Lipstick", heartState: 'empty_heart' },
+    { id: '9', source: item0, price: "21.99", name: "NARS Radiant Creamy Concealer", heartState: 'empty_heart' }
+];
+
+const itemsThird = [
+    { id: '0', source: item0, price: "13.76", name: "INGLOT AMC Eyerliner Gel 5.5g", heartState: 'empty_heart' },
+    { id: '1', source: item1, price: "15.20", name: "L'Oréal Paris Matte Lipstick", heartState: 'empty_heart' },
+    { id: '2', source: item2, price: "9.99", name: "Revlon ColorStay Foundation", heartState: 'empty_heart' },
+    { id: '3', source: item3, price: "12.50", name: "Maybelline Sky High Mascara", heartState: 'empty_heart' },
+    { id: '4', source: item4, price: "7.30", name: "NYX Professional Eyeshadow Palette", heartState: 'empty_heart' },
+    { id: '5', source: item5, price: "18.40", name: "Estée Lauder Double Wear Concealer", heartState: 'empty_heart' },
+    { id: '6', source: item6, price: "20.00", name: "Fenty Beauty Pro Filt'r Primer", heartState: 'empty_heart' },
+    { id: '7', source: item7, price: "10.99", name: "Urban Decay Setting Spray", heartState: 'empty_heart' },
+    { id: '8', source: item8, price: "16.75", name: "Huda Beauty Liquid Matte Lipstick", heartState: 'empty_heart' },
+    { id: '9', source: item9, price: "21.99", name: "NARS Radiant Creamy Concealer", heartState: 'empty_heart' }
+];
 
 function BestSellers(){
     const [tabPressed, setTabPressed] = useState('1');
@@ -50,7 +98,7 @@ function BestSellers(){
                 <button id="skinButton" style={{backgroundColor: pressed2, color: textColor2}} onClick={() => buttonClicked(2)} className='button'>Skin</button>
                 <button id="fragnanceButton" style={{backgroundColor: pressed3, color: textColor3}} onClick={() => buttonClicked(3)} className='button'>Fragnance</button>
             </div>
-            {tabPressed == '1' ? <MakeupItems /> : tabPressed == '2' ? <SkinItems /> : <FragnanceItems />}
+            {tabPressed == '1' ? <MakeupItems arrays={items} /> : tabPressed == '2' ? <MakeupItems arrays={itemsSecond} /> : <MakeupItems arrays={itemsThird} />}
         </div>
     )
 }
--- src/components/MakeupItems.js
+++ src/components/MakeupItems.js
@@ -3,16 +3,6 @@ import React, {useState, useEffect } from 'react';
 import empty_heart from '../assets/heart-images/empty_heart.png';
 import black_red_heart from '../assets/heart-images/black_red_heart.png';
 import fully_red from '../assets/heart-images/fully_red.png';
-import item0 from '../assets/makeup_items/item0.PNG';
-import item1 from '../assets/makeup_items/item1.PNG';
-import item2 from '../assets/makeup_items/item2.PNG';
-import item3 from '../assets/makeup_items/item3.PNG';
-import item4 from '../assets/makeup_items/item4.PNG';
-import item5 from '../assets/makeup_items/item5.PNG';
-import item6 from '../assets/makeup_items/item6.PNG';
-import item7 from '../assets/makeup_items/item7.PNG';
-import item8 from '../assets/makeup_items/item8.PNG';
-import item9 from '../assets/makeup_items/item9.PNG';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
@@ -25,25 +15,13 @@ function shuffleArray(array) {
     return array;
 }
 
-const items = [
-    { id: '0', source: item0, price: "13.76", name: "INGLOT AMC Eyerliner Gel 5.5g", heartState: 'empty_heart' },
-    { id: '1', source: item1, price: "15.20", name: "L'Oréal Paris Matte Lipstick", heartState: 'empty_heart' },
-    { id: '2', source: item2, price: "9.99", name: "Revlon ColorStay Foundation", heartState: 'empty_heart' },
-    { id: '3', source: item3, price: "12.50", name: "Maybelline Sky High Mascara", heartState: 'empty_heart' },
-    { id: '4', source: item4, price: "7.30", name: "NYX Professional Eyeshadow Palette", heartState: 'empty_heart' },
-    { id: '5', source: item5, price: "18.40", name: "Estée Lauder Double Wear Concealer", heartState: 'empty_heart' },
-    { id: '6', source: item6, price: "20.00", name: "Fenty Beauty Pro Filt'r Primer", heartState: 'empty_heart' },
-    { id: '7', source: item7, price: "10.99", name: "Urban Decay Setting Spray", heartState: 'empty_heart' },
-    { id: '8', source: item8, price: "16.75", name: "Huda Beauty Liquid Matte Lipstick", heartState: 'empty_heart' },
-    { id: '9', source: item9, price: "21.99", name: "NARS Radiant Creamy Concealer", heartState: 'empty_heart' }
-];
 
-const items1 = shuffleArray(items);
-const items2 = shuffleArray(items);
-const items3 = shuffleArray(items);
 
-console.log("this is a first random array: " + items1[0].price);
+ //const items1 = shuffleArray([...items]);
+// const items2 = shuffleArray(items);
+// const items3 = shuffleArray(items);
 
+// console.log("items2: " + items2[0].name);
 
 const itemRatings = [];
 for(let i=0; i < 10; i++){
@@ -96,6 +74,34 @@ const FiveStarRating = () =>
 </div>;
 
 function MakeupItems({arrays}){
+    console.log("testing " + arrays);
+    // const [currentArray, setCurrentArray] = useState(items1);
+ 
+    // if(arrays == 1){
+    //     setCurrentArray(items1);
+    //     console.log("equal to 1");
+    // }
+    // else if(arrays == 2){
+    //     setCurrentArray(items2);
+    //     console.log("equal to 2");
+    // }
+    // else if(arrays == 3){
+    //     setCurrentArray(items3);
+    //     console.log("equal to 3");
+    // }
+    // let currentArray = items1;
+    // if(arrays == 1){
+    //     let currentArray = items1;
+    // }
+    // else if(arrays == 2){
+    //     let currentArray = items2;
+        
+    // }
+
+    // console.log("currentArray: " + currentArray);
+    // console.log("items1: " + items1)
+   
+    
     const [heartType, setHeartType] = useState(empty_heart);
         console.log("testing an array transfer: " + {arrays});
 
@@ -113,10 +119,9 @@ function MakeupItems({arrays}){
 
     return(
         <div id="makeup-container">
-            {items.map((item, index) => 
+            {arrays.map((item, index) => 
                 <div onMouseEnter={makeHeartRed} onMouseLeave={makeHeartTrans} className='eachItemContainer'>
-                    <img className='itemImage' src={item.source} />
-                    
+                    <img className='itemImage' alt="item" src={item.source} />
                     {/* <img id={"heart"+item.id} onMouseEnter={makeHeartFullyRed} onMouseLeave={makeHeartRed} src={heartType} alt="a heart like button" className='heartContainer' /> */}
                     <div className='heartContainer'></div>
                     <div className='itemInfo'>
