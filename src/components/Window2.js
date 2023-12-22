ffb8faa2d61f9825b10dc3265710146aff8f939f
Jevgenijs Kozuhovs <jkozuhovs@gmail.com>
2023-12-23 01:23:35 +0200

    made changes to window2

--- src/components/Window2.js
+++ src/components/Window2.js
@@ -1,10 +1,12 @@
 import "./Window2.css";
+import window2poster1 from  "../assets/navbar/window2poster1.PNG";
+import window2poster2 from "../assets/navbar/window2poster2.PNG";
 
 function Window2(){
     return(
         <div class="window2-container">
             <div className="window2-menu">
-                <div className="window2-types-perfume window1-menu-column">
+                <div className="window2-types-perfume window2-menu-column">
                     <ul>
                         <li>PERFUME & EAU DE PERFUM</li>
                         <li>EAU DE TOILETTE</li>
@@ -17,7 +19,7 @@ function Window2(){
                         <li>TRAVEL MINIS</li>
                     </ul>
                 </div>
-                <div className="window2-home-fragnance window1-menu-column">
+                <div className="window2-home-fragnance window2-menu-column">
                     <h3>HOME FRAGNANCE</h3>
                     <ul>
                         <li>Candles</li>
@@ -25,8 +27,18 @@ function Window2(){
                         <li>Room Sprays</li>
                     </ul>
                 </div>
-                <div className="window1-menu-column"></div>
-                <div className="window1-menu-column"></div>
+                <div className="window2-menu-column">
+                    <h3>FRANRANCE FINDER</h3>
+                    <img alt="advent calendars" src={window2poster1}></img>
+                    <p>Discover your new signature scent with our Fragnance & Perfume Finder</p>
+                    <button>Shop Now</button>
+                </div>
+                <div className="window2-menu-column">
+                <h3>FRANRANCE FINDER</h3>
+                    <img alt="advent calendars" src={window2poster2}></img>
+                    <p>A contemporary award-winning British fragrance brand, powered by flowers</p>
+                    <button>Shop Now</button>
+                </div>
             </div>
         </div>
 
